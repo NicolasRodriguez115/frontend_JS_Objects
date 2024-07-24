62d81e9e80b9129ad69daeada5a3e0c208d7bc58
Nicolas Rodriguez <nr115935@gmail.com>
2024-07-24 12:47:48 -0400

    Delete whiteboard.py

--- whiteboard.py
+++
@@ -1,4 +0,0 @@
-# Create a function that given a list which represents street lights given as a parameter(l_street), determine if an outage has occurred.
-#  A street with a total number of “F” greater than or equal to 2 returns “Outage”, anything below returns “Power”
-# Example Input: [ ‘T’, ‘F’, ‘F’, ‘F’ ]				
-# Example Output: “Outage”
